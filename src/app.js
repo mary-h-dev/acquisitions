c72897717ef5a6be1551486323a93383dfe538fc
mary-h-dev <Einesheikh_12@yahoo.com>
2025-09-23 14:00:01 +0100

    implement users crud

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRoutes from '#routes/auth.routes.js';
 import securityMiddleware from '#middleware/security.middleware.js';
+import usersRoutes from '#routes/users.routes.js';
+
 
 
 const app = express();
@@ -46,6 +48,13 @@ app.get('/api', (req, res) => {
 });
 
 app.use('/api/auth', authRoutes);
+
+app.use('/api/users', usersRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({ error: 'Route not found' });
+});
+
 
 
 
--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,17 @@
 import 'dotenv/config';
 
+
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
+import {  neonConfig } from '@neondatabase/serverless';
+
+
+if (process.env.NODE_ENV === 'development') {
+  neonConfig.fetchEndpoint = 'http://neon-local:5432/sql';
+  neonConfig.useSecureWebSocket = false;
+  neonConfig.poolQueryViaFetch = true;
+}
+
 
 const sql = neon(process.env.DATABASE_URL);
 
